d77104074a52187e8cfc6ee4e731ce86ad345e22
Yadikaer Yasheng <yadikae1@umbc.edu>
2015-03-21 18:45:38 -0400

    update

--- test.php
+++ test.php
@@ -1,19 +1,5 @@
-<html>
+<?php
 
-<link rel="stylesheet" type="text/css" href="css/main.css">
+echo "string";
 
-<body>
-<img src="src/umbclogo.png" width="300" height="116">
-</body>
-  
-<nav>  
-  <ul>
-    <li><a href="#home">Home</a></li>
-    <li><a href="#news">News</a></li>
-    <li><a href="#contact">Contact</a></li>
-    <li><a href="#about">About</a></li>
-  </ul>
-</nav>
-
-
-</html>
+?>
