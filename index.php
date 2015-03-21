0d3b1a9214d709db12faa40beef131fd6b8a0147
Yadikaer Yasheng <yadikae1@umbc.edu>
2015-03-21 13:45:18 -0400

    setup

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+
+<link rel="stylesheet" type="text/css" href="css/main.css">
+
+<body>
+<img src="src/umbclogo.png" width="300" height="116">
+</body>
+  
+<nav>  
+  <ul>
+    <li><a href="#home">Home</a></li>
+    <li><a href="#news">News</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#about">About</a></li>
+  </ul>
+</nav>
+
+
+</html>
