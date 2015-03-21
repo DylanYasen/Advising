5fc8de9d20e6b5343cf1bc58e94d41373a3fcba4
Yadikaer Yasheng <yadikae1@umbc.edu>
2015-03-21 19:17:57 -0400

    update

--- src/CommonMethods.php
+++ src/CommonMethods.php
@@ -16,7 +16,7 @@ class Common
 	
 	function connect($db)// connect to MySQL
 	{
-		$conn = @mysql_connect("studentdb.gl.umbc.edu", "yadikae1", "yadikae1") or die("Could not connect to MySQL");
+		$conn = @mysql_connect("studentdb.gl.umbc.edu", "yadikae1", "yadikae1","AdvisingTimeTable") or die("Could not connect to MySQL");
 		$rs = @mysql_select_db($db, $conn) or die("Could not connect select $db database");
 		$this->conn = $conn; 
 	}
