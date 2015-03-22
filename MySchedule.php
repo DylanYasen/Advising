ab087965cce2072721aca180f3ca30ee21645d2d
Yadikaer Yasheng <yadikae1@umbc.edu>
2015-03-22 11:54:59 -0400

    update

--- MySchedule.php
+++ MySchedule.php
@@ -84,7 +84,7 @@ echo "<body>";
         echo "<th>";
         echo $time;
 
-            for($i = 0; $i < 4; $i++){
+            for($j = 0; $j < 4; $j++){
                 echo "<td>";
                 echo "</td>";
             }
