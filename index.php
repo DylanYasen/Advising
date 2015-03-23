26c1f1580f19eeab188b177256b78a706f04e02b
Yadikaer Yasheng <yadikae1@umbc.edu>
2015-03-23 15:42:23 -0400

    fix

--- index.php
+++ index.php
@@ -61,9 +61,9 @@
       <form class="form-signin"  Method ="post" ACTION = "login.php">
         <h2 class="form-signin-heading">Please sign in</h2>
         <label for="inputUsername" class="sr-only">Email address</label>
-        <input type="username" id="inputUsername" class="form-control" placeholder="Username" required autofocus>
+        <input type="text" id="inputUsername" class="form-control" placeholder="Username" required autofocus>
         <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <input type="text" id="inputPassword" class="form-control" placeholder="Password" required>
         <div class="checkbox">
           <label>
             <input type="checkbox" value="remember-me"> Remember me
