3a8926bcffa08f76feb9bdad48f30899236f6bca
Yadikaer Yasheng <yadikae1@umbc.edu>
2015-03-23 16:27:50 -0400

    fix

--- login.php
+++ login.php
@@ -1,12 +1,12 @@
 <?php
 
 include('src/Validator.php');
-	//var_dump($_POST);
+	var_dump($_POST);
 
 	$debug = true;
 	$VALIDATOR = new Validator($debug); 
 
-	if($VALIDATOR->Validate( $_POST['inputUsername'],$_POST['inputPassword']))
+	if($VALIDATOR->Validate( $_POST['username'],$_POST['password']))
 		 header("Location:index.php");
 
 	else
