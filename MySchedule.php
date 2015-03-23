1dfac464132864df62eebfac6c201e6ac94ebba3
Yadikaer Yasheng <yadikae1@umbc.edu>
2015-03-23 17:26:20 -0400

    d

--- MySchedule.php
+++ MySchedule.php
@@ -9,6 +9,8 @@
   <!-- Optional theme -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
 
+    <link href="css/main.css" rel="stylesheet">
+
 </head>
 
 
