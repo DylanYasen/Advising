0edc6ea327e65c83123fd9e2dc10163ec6f516c3
Yadikaer Yasheng <yadikae1@umbc.edu>
2015-03-23 16:00:52 -0400

    get

--- index.php
+++ index.php
@@ -61,9 +61,9 @@
       <form class="form-signin" action = "login.php" method ="post">
         <h2 class="form-signin-heading">Please sign in</h2>
         <label for="inputUsername" class="sr-only">Email address</label>
-        <input type="text" id="inputUsername" class="form-control" placeholder="Username" required autofocus>
-        <label for="inputPassword" class="sr-only">Password</label>
-        <input type="text" id="inputPassword" class="form-control" placeholder="Password" required>
+        <input type="text" name = "username" id="inputUsername" class="form-control" placeholder="Username" required autofocus>
+        <label for="inputPassword"  class="sr-only">Password</label>
+        <input type="text" id="inputPassword" name = "password" class="form-control" placeholder="Password" required>
         <div class="checkbox">
           <label>
             <input type="checkbox" value="remember-me"> Remember me
