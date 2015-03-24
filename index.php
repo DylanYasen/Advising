4b09a87f1d29695a8dfc02940f038b7c28ee4328
Yadikaer Yasheng <yadikae1@umbc.edu>
2015-03-24 17:40:04 -0400

    remove check box

--- index.php
+++ index.php
@@ -54,11 +54,6 @@
         <input type="text" name = "username" id="inputUsername" class="form-control" placeholder="Username" required autofocus>
         <label for="inputPassword"  class="sr-only">Password</label>
         <input type="password" id="inputPassword" name = "password" class="form-control" placeholder="Password" required>
-        <div class="checkbox">
-          <label>
-            <input type="checkbox" value="remember-me"> Remember me
-          </label>
-        </div>
         <button class="btn btn-lg btn-success btn-block" type="submit" >Sign in</button>
       </form>
 
