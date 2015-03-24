dc6acde750d5c212400587da45d3495001b54e69
Yadikaer Yasheng <yadikae1@umbc.edu>
2015-03-24 16:45:48 -0400

    sa

--- printPage.php
+++ printPage.php
@@ -1,6 +1,66 @@
 
 <?php 
 var_dump($_POST);
+
+ include('src/CommonMethods.php');
+
+    $debug = true;
+    $COMMON = new Common($debug);
+
+    $advisorID = $_POST['advisorID'];
+    $day = $_POST['day'];
+    //var_dump($advisorID);
+
+    // ---- individual appointments ---- //
+    $sql = "SELECT * FROM Appointment WHERE Advisor_ID = '$advisorID' AND Day = '$day' ORDER BY StartTime ASC";
+    $rs = $COMMON->executeQuery($sql,$_SERVER["SCRIPT_NAME"]);
+
+    $apts = $COMMON->getDataArray($rs);
+    //var_dump($apts);
+
+    // ---- group appointments ---- //
+    $sql = "SELECT * FROM AppointmentGroup WHERE Advisor_ID = '$advisorID' AND Day = '$day' ORDER BY StartTime ASC";
+    $rs = $COMMON->executeQuery($sql,$_SERVER["SCRIPT_NAME"]);
+
+    $groupApts = $COMMON->getDataArray($rs);
+
+    var_dump($apts);
+    var_dump($groupApts)
+
+    echo "<ul>";
+    echo "Individual Advising";
+    foreach ($apts as $apt) {
+
+    	$startTime = $apt[2];
+		$startTime = substr($startTime,0,5);
+
+		$endTime = $apt[3];
+		$endTime = substr($endTime,0,5);
+
+		$studentID = $apt[5];
+
+		// get student info
+		$sql = "SELECT * FROM Student WHERE ID = '$studentID'";
+		$rs = $COMMON->executeQuery($sql,$_SERVER["SCRIPT_NAME"]);
+
+		$studentInfo = $COMMON->getDataArray($rs);
+		$studentInfo = $studentInfo[1];
+		$studentID = $studentInfo[0];
+		$studentFirstname = $studentInfo[1];
+		$studentLastname = $studentInfo[2];
+		$studentFullname = $studentFirstname." ".$studentLastname;
+		$studentMajor = $studentInfo[3];
+		$studentRank = $studentInfo[4];
+
+		echo "<li>Time: ".$startTime." - ".$endTime."		";
+    	echo "Student Name: ".$studentFullname."		";
+    	echo "ID: ".$studentID."		";
+    	echo "Major: ".$studentMajor."		";
+    	echo "Rank: ".$studentRank."</li>";
+    	echo "<br>"; 
+    	}
+     echo "</ul>";
+
 /*
 class Printer
 {	
