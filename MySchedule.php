c4d6cf968fa6ae4549353f32b4245524cda50935
Yadikaer Yasheng <yadikae1@umbc.edu>
2015-03-24 12:40:23 -0400

    d

--- MySchedule.php
+++ MySchedule.php
@@ -315,7 +315,7 @@ echo"</body>";
                                             $studentMajor = $studentInfo[4];
                                             $studentRank = $studentInfo[5];
 
-                                            var_dump($studentFullname)
+                                            var_dump($studentFullname);
 
                                             echo "<li>".$startTime." - ".$endTime."</li>";
                                             
