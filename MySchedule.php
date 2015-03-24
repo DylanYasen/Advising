a693a2e8b51c29a76e987547dc769a9159d994de
Yadikaer Yasheng <yadikae1@umbc.edu>
2015-03-24 15:29:55 -0400

    asd

--- MySchedule.php
+++ MySchedule.php
@@ -381,13 +381,11 @@ echo"</body>";
                                                 $studentMajor = $studentInfo[3];
                                                 $studentRank = $studentInfo[4];
 
-                                                echo "Name: $studentFullname <br /> 
+                                                echo "'Name: $studentFullname <br /> 
                                                                            ID: $studentID <br />
                                                                            Major: $studentMajor <br />
-                                                                           Rank: $studentRank <br /><br />   ";
+                                                                           Rank: $studentRank <br /><br />   '";
                                                            
-
-
 
                                             }
                                             echo ">Detail</button>";
