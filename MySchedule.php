16e73ea82165cdee6779c41c86ba0c29f7d15823
Yadikaer Yasheng <yadikae1@umbc.edu>
2015-03-24 13:41:37 -0400

    ds

--- MySchedule.php
+++ MySchedule.php
@@ -322,7 +322,7 @@ echo"</body>";
                                                     echo "<button type='button' class='btn btn-lg btn-info' data-toggle='popover' data-placement='bottom' title='Student Info'"; 
 
                                                         // get student info
-                                                        echo 'data-content=sfgasdfasdfasdfasdfasdfas\nfasdfasdf';
+                                                        echo 'data-content=sfgasdfasdfasdfasdfasdfas/nfasdfasdf';
 
                                                         echo "Name: ".$studentFullname;
                                                         echo "ID: ".$studentID;
