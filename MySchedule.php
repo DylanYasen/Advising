6305c7b6d5f0f4896240cb48875ebb3f7c8bf3d8
Yadikaer Yasheng <yadikae1@umbc.edu>
2015-03-24 12:10:37 -0400

    gf

--- MySchedule.php
+++ MySchedule.php
@@ -302,14 +302,13 @@ echo"</body>";
 
                                             echo "<li >".$startTime." - ".$endTime."</li>";
                                             
-
-                                            echo "<button type='button' class='btn btn-xs btn-info' data-toggle='popover' title='Popover title'"; 
-                                                echo "<div class = 'container'>";
-                                                    echo "<div class == 'col-md-12'";
+                                            echo "<div class = 'container'>";
+                                                echo "<div class == 'col-md-12'";
+                                                    echo "<button type='button' class='btn btn-xs btn-info' data-toggle='popover' title='Popover title'"; 
                                                         echo "data-content='And heres some amazing content. Its very engaging. Right?'>";
-                                                    echo "</div>";
+                                                    echo "Detail</button>";
                                                 echo "</div>";
-                                            echo "Detail</button>";
+                                            echo "</div>";
 
                                             //echo "<a data-container='body' data-toggle='popover' data-placement='bottom' data-trigger='hover'>StudentID:$apt[5]</a>";
                                             echo "<br>";
