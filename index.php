13921cd0f6bd1e084a506b002960f61d5e0fafc7
Yadikaer Yasheng <yadikae1@umbc.edu>
2015-03-24 17:17:37 -0400

    asd

--- index.php
+++ index.php
@@ -40,20 +40,6 @@
      
       <ul class="nav navbar-nav navbar-right">
         <li><a href="#">Login</a></li>
-
-        <!--
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Menu <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="index.php">Home</a></li>
-            <li><a href="MySchedule.php">My Schedule</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Log Out</a></li>
-          </ul>
-        </li>
-        !-->
-        
       </ul>
     </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
@@ -78,41 +64,6 @@
 
     </div> <!-- /container -->
 
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <!--<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script> -->
-
-<?php
-/*
-// Stage 1, vardump, watch all values passed
-var_dump($_POST);  echo("<br>");
-
-include('src/CommonMethods.php');
-$debug = true;
-$COMMON = new Common($debug); // common methods
-
-$sql = "SELECT ID, StudentName, StartTime, EndTime, AptType FROM AdvisingTimeTable";
-$result = $COMMON->executeQuery($sql,$_SERVER["SCRIPT_NAME"]);
-    echo "'$result'";
-
-    while($row = mysql_fetch_row($result)) {
-        //echo "id: " . $row["id"]. " - Name: " . $row["StudentName"]. " " . $row["StartTime"]." " . $row["EndTime"]. "<br>";
-      
-      echo("<tr>");
-      foreach ($row as $element)
-       echo("<td>".$element."</td><br>");
-      echo("</tr>");
-    }
-
-//$name = 
-
-//$sql = "insert into test_data (`StudentName`, `StartTime`, `EndTime`,'AptType') values ('1', '2', '2','2')";
-//$rs = $COMMON->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
-
-var_dump($_POST);  echo("<br>");
-
-
-*/
-?>
 
 <!-- Load javascript required for Bootstrap animation-->
 <script src="https://code.jquery.com/jquery.js"></script>
